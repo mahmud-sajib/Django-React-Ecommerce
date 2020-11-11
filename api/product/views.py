5d50c049ad4a0d618825c2258589208953077944
unknown <shout.mahmud@gmail.com>
2020-11-11 14:10:46 +0600

    Api created for user and added Braintree Payment Gateway

--- api/product/views.py
+++ api/product/views.py
@@ -3,9 +3,10 @@ from .serializers import ProductSerializer
 from .models import Product
 
 # Create your views here.
+
+# Product View
 class ProductViewSet(viewsets.ModelViewSet):
     # Operations to be performed
     queryset = Product.objects.all().order_by('id')
     # Class responsible for serializing the data 
     serializer_class = ProductSerializer 
-
